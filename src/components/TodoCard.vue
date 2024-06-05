9b4e70125f515d21170290ad5239cad05b933d14
BagasKelana <hilalbagas@gmail.com>
2024-06-05 21:56:19 +0700

    feat: action card

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -1,32 +1,32 @@
 <template>
   <v-card
-    theme="dark"
     class="ma-4"
     rounded="lg"
-    variant="tonal"
-    :color="isDone ? '#00E676' : props.color"
+    :variant="isDone ? 'elevated' : 'tonal'"
+    :color="colorPallet"
   >
     <v-card-item>
       <v-card-title class="text-body-2 d-flex align-center">
         <v-icon icon="mdi-calendar" start></v-icon>
 
-        <span class="text-medium-emphasis font-weight-bold">
-          {{ props.date }} - {{ props.time }}
+        <span class="font-weight-bold">
+          {{ formatDate }} - {{ time }}
+          {{ isDone ? "(Done)" : isOverdue ? "(Overdue Todo)" : "" }}
         </span>
 
         <v-spacer></v-spacer>
-        <!--  -->
+
         <v-icon
-          v-show="props.important"
+          v-show="!!props.important"
           size="large"
           icon="mdi-star-check"
         ></v-icon>
       </v-card-title>
-      <!-- title -->
+
       <div class="py-2">
         <div class="text-h6">{{ props.title }}</div>
-        <!-- description -->
-        <div class="font-weight-light text-medium-emphasis">
+
+        <div class="font-weight-light">
           {{ props.description }}
         </div>
       </div>
@@ -35,63 +35,130 @@
     <v-divider></v-divider>
 
     <div class="pa-4 d-flex align-center">
-      <!-- category or place iteration -->
-
-      <v-chip-group>
+      <div>
         <v-chip
-          variant="tonal"
-          v-for="{ id, category_name, color, icon } in props.place"
+          class="ma-1"
+          size="small"
+          variant="flat"
+          v-for="{ value, color, icon } in props.categories"
           :prepend-icon="icon"
           :color="color"
-          :key="id"
+          :key="value"
         >
-          {{ category_name }}
+          {{ value }}
         </v-chip>
-      </v-chip-group>
+      </div>
 
       <v-spacer></v-spacer>
       <!-- menu toggle (edit, delete, etc) -->
-      <v-btn icon="mdi-dots-horizontal" variant="text"></v-btn>
-      <!-- done btn -->
-      <v-btn class="text-none" prepend-icon="mdi-check" variant="text" border>
-        Done
-      </v-btn>
+      <v-menu>
+        <template v-slot:activator="{ props: menu }">
+          <v-tooltip location="top center">
+            <template v-slot:activator="{ props: tooltip }">
+              <v-btn
+                rounded
+                icon="mdi-dots-horizontal"
+                variant="text"
+                v-bind="mergeProps(menu, tooltip)"
+              >
+              </v-btn>
+            </template>
+            <span>Action</span>
+          </v-tooltip>
+        </template>
+
+        <v-list>
+          <v-list-item
+            class="text-subtitle-6"
+            v-for="item in items"
+            :key="item.text"
+            :value="item"
+            @click="item.action"
+          >
+            <template v-slot:prepend>
+              <v-icon size="small" :icon="item.icon"></v-icon>
+            </template>
+
+            <v-list-item-subtitle>{{ item.text }}</v-list-item-subtitle>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </div>
   </v-card>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { useDeleteModal, useEditModal } from "../stores/useModal";
+import { useTodoStore } from "../stores/useTodoStore";
+import { computed, mergeProps, onMounted, ref, watchEffect } from "vue";
 
-import { defineProps } from "vue";
+const props = defineProps([
+  "id",
+  "slug",
+  "title",
+  "description",
+  "date",
+  "time",
+  "categories",
+  "color",
+  "isDone",
+  "isOverdue",
+  "important",
+]);
 
-// Define the props with validation
-const props = defineProps({
-  slug: { type: String, required: true },
-  title: { type: String, required: true },
-  description: { type: String, default: "" },
-  date: { type: String, default: "" },
-  time: { type: String, default: "" },
-  place: {
-    type: Array,
-    default: () => [],
-    validator: (value) => {
-      return value.every(
-        (item) =>
-          typeof item.id === "string" &&
-          typeof item.category_name === "string" &&
-          ["home", "work", "personal", "health/fitness", "education"].includes(
-            item.category_name
-          ) &&
-          typeof item.icon === "string" &&
-          typeof item.color === "string"
-      );
+const { openModal } = useDeleteModal();
+const { openModal: openEditModal } = useEditModal();
+const { setIsDone, markImportantTodo } = useTodoStore();
+
+onMounted(() => {
+  let date = new Date(props.date);
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  formatDate.value = date.toLocaleDateString("en-US", options);
+});
+
+const formatDate = ref("");
+
+watchEffect(() => {
+  let date = new Date(props.date);
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  formatDate.value = date.toLocaleDateString("en-US", options);
+});
+
+const colorPallet = computed(() => {
+  return props.isDone
+    ? "#43A047"
+    : props.isOverdue
+    ? "#E53935"
+    : props.important
+    ? "primary"
+    : "";
+});
+
+const handleDoneAction = () => {
+  setIsDone(props.id);
+};
+const handleImportantAction = () => {
+  markImportantTodo(props.id);
+};
+const handleEditAction = () => {
+  openEditModal(props.id);
+};
+const handleDeleteAction = () => {
+  openModal(props.id);
+};
+
+const items = computed(() => {
+  const value = [
+    {
+      text: !props.isDone ? "Done" : "Undone",
+      icon: !props.isDone ? "mdi-check" : "mdi-close",
+      action: handleDoneAction,
     },
-  },
-  color: { type: String, default: "" },
-  isDone: { type: Boolean, default: false },
-  isOverdue: { type: Boolean, default: false },
-  important: { type: Boolean, default: false },
+    { text: "Important", icon: "mdi-star", action: handleImportantAction },
+    { text: "Edit", icon: "mdi-pencil-outline", action: handleEditAction },
+    { text: "Delete", icon: "mdi-delete-forever", action: handleDeleteAction },
+  ];
+  return value;
 });
 </script>
 
