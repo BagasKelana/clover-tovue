53063b63d5bfe88ddec407edc38fd518c7d5fe51
BagasKelana <hilalbagas@gmail.com>
2024-06-05 22:01:35 +0700

    feat(router) : today todo route

---
+++ src/pages/today.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="text-display-1 pa-4">Today Todo</h1>
+  <TodoCard v-for="itam in data" v-bind="itam" :key="itam.id" />
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { useTodoStore } from "../stores/useTodoStore";
+import { todayTodos } from "../helpers/helpers";
+
+const { loadTodos, getTodos } = useTodoStore();
+
+onMounted(() => {
+  loadTodos();
+  data.value = todayTodos(getTodos());
+});
+
+const data = ref(null);
+</script>
