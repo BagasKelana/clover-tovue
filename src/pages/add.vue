d5c410ef9646988ddd3d8ac3e0ccd803e12fae48
BagasKelana <hilalbagas@gmail.com>
2024-06-05 23:56:53 +0700

    fix: active nav

--- src/pages/add.vue
+++ src/pages/add.vue
@@ -1,7 +1,9 @@
 <template>
   <v-card class="px-4 py-4">
-    <div class="d-flex align-center mb-6  ga-4">
-      <v-btn append-icon="mdi-arrow-left" variant="text" to="/">Go Back</v-btn>
+    <div class="d-flex align-center mb-6 ga-4">
+      <v-btn append-icon="mdi-arrow-left" variant="text" href="/"
+        >Go Back</v-btn
+      >
       <h1>ADD TODO</h1>
     </div>
 
