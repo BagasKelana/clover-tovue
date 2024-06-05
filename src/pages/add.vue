c1e7cf977cf947f839b691a3573a515874ea886e
BagasKelana <hilalbagas@gmail.com>
2024-06-05 21:58:18 +0700

    feat(router) : add todo route

---
+++ src/pages/add.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card class="px-4 py-4">
+    <div class="d-flex align-center mb-6  ga-4">
+      <v-btn append-icon="mdi-arrow-left" variant="text" to="/">Go Back</v-btn>
+      <h1>ADD TODO</h1>
+    </div>
+
+    <FormTodo />
+  </v-card>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
